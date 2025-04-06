85b30da42b55632ba4e51bb2fc0ea912dd7d98e7
Astarag Mohapatra <athekunal@astarags-mbp.lan>
2025-04-05 19:50:31 -0700

    working sec filings api

--- mcp_sec_filings/datamodels.py
+++ mcp_sec_filings/datamodels.py
@@ -3,6 +3,7 @@ import pydantic_settings
 from typing import Annotated, Any
 import datetime
 
+import re
 
 from mcp_sec_filings import constants
 
@@ -22,4 +23,33 @@ class SECFilingsRequest(pydantic.BaseModel):
     def model_post_init(self, __context: Any) -> None:
         self.ticker = self.ticker.upper()
 
+def validate_date(date_str: str) -> str:
+    try:
+        datetime.datetime.strptime(date_str,"%Y-%m-%d")
+    except:
+        raise ValueError(f"The date format YYYY-MM-DD failed for {date_str=}")
+    return date_str
 
+class AccessionNumElem(pydantic.BaseModel):
+    accession_num: str
+    no_dashes_accession_num: str | None = None
+    filing_name: str
+    filing_date: Annotated[str,"Filing Date",pydantic.AfterValidator(validate_date)]
+    report_date: Annotated[str,"Report Date",pydantic.AfterValidator(validate_date)]
+    no_dashes_report_date: str | None = None
+
+    def model_post_init(self, __context: Any) -> None:
+        self.no_dashes_accession_num = re.sub("-", "", self.accession_num)
+        self.no_dashes_report_date = "".join(self.report_date.split("-"))
+
+class HTMLURLList(pydantic.BaseModel):
+    rgld_cik: int
+    html_url: str
+    filing_name: str
+
+    @classmethod
+    def from_cik_accnum_ticker(cls, cik: str, acc_num: "AccessionNumElem", ticker: str) -> "HTMLURLList":
+        rgld_cik = int(cik.lstrip("0"))
+        html_url = f"{constants.SEC_EDGAR_URL}/{rgld_cik}/{acc_num.no_dashes_accession_num}/{ticker.lower()}-{acc_num.no_dashes_report_date}.htm"
+        filing_name = acc_num.filing_name
+        return cls(rgld_cik=rgld_cik, html_url=html_url, filing_name=filing_name)
--- mcp_sec_filings/sec_filings.py
+++ mcp_sec_filings/sec_filings.py
@@ -1,11 +1,11 @@
-from typing import List
 import re
 import pandas as pd
 from datetime import datetime
 from typing import Union
-import requests
 import pdfkit
 import os
+import httpx
+from loguru import logger
 from mcp_sec_filings import constants, datamodels
 
 os.makedirs(constants.BASE_DIR,exist_ok=True)
@@ -15,7 +15,7 @@ def _search_url(cik: Union[str, int]) -> str:
     url = f"{constants.SEC_SEARCH_URL}?{search_string}"
     return url
 
-def get_cik_by_ticker(ticker: str) -> str:
+async def get_cik_by_ticker(ticker: str) -> str:
     """Gets a CIK number from a stock ticker by running a search on the SEC website."""
     request_settings = datamodels.RequestSettings()
     url = _search_url(ticker)
@@ -23,72 +23,73 @@ def get_cik_by_ticker(ticker: str) -> str:
         "User-Agent": f"{request_settings.company_name} {request_settings.email}",
         "Content-Type": "text/html",
     }
-    response = requests.get(url, stream=True, headers=headers)
-    response.raise_for_status()
-    cik_re = re.compile(r".*CIK=(\d{10}).*")
-    results = cik_re.findall(response.text)
-    assert len(results) == 1, f"Expected 1 result match, but got {response.text=} and {results=}"
+    
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+        cik_re = re.compile(r".*CIK=(\d{10}).*")
+        results = cik_re.findall(response.text)
+
     return str(results[0])
 
-
-def sec_save_pdfs(sec_filings_request: datamodels.SECFilingsRequest) -> None:
-    cik = get_cik_by_ticker(sec_filings_request.ticker)
-    rgld_cik = int(cik.lstrip("0"))
-    ticker_year_path = os.path.join(constants.BASE_DIR,f"{sec_filings_request.ticker}-{sec_filings_request.year}")
-    os.makedirs(ticker_year_path, exist_ok=True)
+async def get_metadata_from_ticker(sec_filings_request: datamodels.SECFilingsRequest) -> tuple[str, dict[str, list[str]] | None]:
+    cik = await get_cik_by_ticker(sec_filings_request.ticker)
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     url = constants.SEC_CIK_URL.format(cik=cik)
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        response = await client.get(url, headers=headers)
+        try:
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            logger.error(f"Error fetching data for {sec_filings_request.model_dump_json()}: {exc}")
+            return cik, None
+
         json_data = response.json()
-    else:
-        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
 
-    form_lists = []
-    filings = json_data["filings"]
-    recent_filings = filings["recent"]
-    sec_form_names = []
-    for acc_num, form_name, filing_date, report_date in zip(
+    return cik, json_data.get("filings", {}).get("recent")
+
+def get_accession_list(recent_filings: dict[str,list[str]], sec_filings_request: datamodels.SECFilingsRequest) -> list[datamodels.AccessionNumElem]:
+    acc_nums_list: list[datamodels.AccessionNumElem] = []
+    sec_form_names: list[str] = []
+    for acc_num, filing_name, filing_date, report_date in zip(
         recent_filings["accessionNumber"],
         recent_filings["form"],
         recent_filings["filingDate"],
         recent_filings["reportDate"],
     ):
-        if form_name in forms and report_date.startswith(str(sec_filings_request.year)):
-            if form_name == "10-Q":
+        if filing_name in sec_filings_request.filing_types and report_date.startswith(str(sec_filings_request.year)):
+            if filing_name == "10-Q":
                 datetime_obj = datetime.strptime(report_date, "%Y-%m-%d")
                 quarter = pd.Timestamp(datetime_obj).quarter
-                form_name += str(quarter)
-                if form_name in sec_form_names:
-                    form_name += "-1"
-            no_dashes_acc_num = re.sub("-", "", acc_num)
-            form_lists.append([no_dashes_acc_num, form_name, filing_date, report_date])
-            sec_form_names.append(form_name)
-    process_links = lambda x: "".join(x.split("-"))
+                filing_name += str(quarter)
+                if filing_name in sec_form_names:
+                    filing_name += "-1"
+            acc_nums_list.append(datamodels.AccessionNumElem(accession_num=acc_num,filing_name=filing_name,filing_date=filing_date,report_date=report_date))
+            sec_form_names.append(filing_name)
+    return acc_nums_list
 
-    acc_nums_list = [[fl[0],fl[1],process_links(fl[-1])] for fl in form_lists]
-    
-    html_urls = [[f"{constants.SEC_EDGAR_URL}/{rgld_cik}/{acc}/{sec_filings_request.ticker.lower()}-{report_date}.htm",filing_type] for acc,filing_type,report_date in acc_nums_list]
-    
-    metadata_json = _convert_html_to_pdfs(html_urls,ticker_year_path)
-    
-    # with open(os.path.join(ticker_year_path,'metadata.json'), 'w') as f:
-    #     json.dump(metadata_json, f)
-    return html_urls, metadata_json, ticker_year_path
+async def sec_save_pdfs(sec_filings_request: datamodels.SECFilingsRequest) -> tuple[list[datamodels.HTMLURLList],str] | None:
+    ticker_year_path = os.path.join(constants.BASE_DIR,f"{sec_filings_request.ticker}-{sec_filings_request.year}")
+    os.makedirs(ticker_year_path, exist_ok=True)
+    cik, recent_filings = await get_metadata_from_ticker(sec_filings_request)
+    if not recent_filings:
+        logger.error(f"Could not retrieve for {sec_filings_request.model_dump()}")
+        return None
+    acc_nums_list = get_accession_list(recent_filings=recent_filings, sec_filings_request=sec_filings_request)
+    html_urls = [datamodels.HTMLURLList.from_cik_accnum_ticker(cik=cik,acc_num=acc_num,ticker=sec_filings_request.ticker) for acc_num in acc_nums_list]
+    _convert_html_to_pdfs(html_urls,ticker_year_path)
 
-def _convert_html_to_pdfs(html_urls,base_path:str):
-    metadata_json = {}
+    return html_urls, os.path.abspath(ticker_year_path)
+
+def _convert_html_to_pdfs(html_urls:list[datamodels.HTMLURLList],base_path:str) -> None:
     for html_url in html_urls:
-        pdf_path = html_url[0].split("/")[-1]
-        # Add the filing type
-        pdf_path = pdf_path.replace(".htm",f"-{html_url[1]}.pdf")
-        # /A for amended is not a valid path
+        pdf_path = html_url.html_url.split("/")[-1]
+        pdf_path = pdf_path.replace(".htm",f"-{html_url.filing_name}.pdf")
         pdf_path = pdf_path.replace("/A","A")
-        metadata_json[pdf_path] = {"languages":["English"]}
         pdf_path = os.path.join(base_path,pdf_path)
-        pdfkit.from_url(html_url[0], pdf_path)
-    return metadata_json
+        pdfkit.from_url(html_url.html_url, pdf_path)
+        logger.info(f"Saved filing {html_url.filing_name} at {pdf_path=}")
