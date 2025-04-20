9a8f52c696fc12087ad1b6d7df5a1d0af0d9afc8
Athe-kunal <athekunal@gmail.com>
2025-04-19 19:00:06 -0700

    async shift

--- mcp_sec_filings/docling_vllm.py
+++ mcp_sec_filings/docling_vllm.py
@@ -3,7 +3,8 @@ import torch
 from docling_core.types.doc import DoclingDocument
 from docling_core.types.doc.document import DocTagsDocument
 from transformers import AutoProcessor, AutoModelForVision2Seq
-from vllm import LLM, SamplingParams
+from vllm import AsyncLLMEngine, SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
 from PIL import Image
 
 from mcp_sec_filings import constants
@@ -21,23 +22,31 @@ def load_model()-> tuple[AutoProcessor, AutoModelForVision2Seq]:
     return processor, model
 
 def load_vllm_model():
-    llm = LLM(model=constants.DOCLING_MODEL_NAME, limit_mm_per_prompt={"image": 1}, gpu_memory_utilization=0.99)
+    llm = AsyncLLMEngine.from_engine_args(
+        AsyncEngineArgs(
+            model = constants.DOCLING_MODEL_NAME,
+            dtype="auto",
+            enforce_eager=True,
+            gpu_memory_utilization=0.9,
+            disable_log_requests=True
+        )
+    )
 
     sampling_params = SamplingParams(
         temperature=0.0,
         max_tokens=constants.MAX_TOKENS)
     return llm, sampling_params
 
-async def convert_single_image_to_md(sempahore:asyncio.Semaphore, llm: LLM, sampling_params: SamplingParams, image: Image.Image)-> str | None:
-    async with sempahore:
-        try:
+async def convert_single_image_to_md(sempahore:asyncio.Semaphore, llm: AsyncLLMEngine, sampling_params: SamplingParams, image: Image.Image)-> str | None:
+    try:
+        async with sempahore:
             llm_input = {"prompt": constants.CHAT_TEMPLATE, "multi_modal_data": {"image": image}}
-            output = llm.generate([llm_input], sampling_params=sampling_params)[0]
+            output = await llm.generate([llm_input], sampling_params=sampling_params)[0]
             doctags = output.outputs[0].text
             doctags_doc = DocTagsDocument.from_doctags_and_image_pairs([doctags], [image])
             doc = DoclingDocument(name="Document")
             doc.load_from_doctags(doctags_doc)
             return doc.export_to_markdown()
-        except Exception as e:
-            return None
+    except Exception as e:
+        return None
 
--- mcp_sec_filings/pdf2md.py
+++ mcp_sec_filings/pdf2md.py
@@ -3,33 +3,39 @@ import asyncio
 import pdf2image
 import pathlib
 from loguru import logger
-from vllm import LLM, SamplingParams
+from vllm import AsyncLLMEngine, SamplingParams
 from mcp_sec_filings import constants, datamodels, docling_vllm
 
-async def convert_single_pdf_to_markdown(semaphore: asyncio.Semaphore, mcpresult_pdf: datamodels.MCPResultsPDF, llm: LLM, sampling_params: SamplingParams):
+async def convert_single_pdf_to_markdown(semaphore: asyncio.Semaphore, mcpresult_pdf: datamodels.MCPResultsPDF, llm: AsyncLLMEngine, sampling_params: SamplingParams):
     images = pdf2image.convert_from_path(mcpresult_pdf.pdf_path)
     tasks = [
         docling_vllm.convert_single_image_to_md(semaphore, llm, sampling_params, image)
         for image in images
     ]
-    md_list = await asyncio.gather(*tasks)
+    md_list = await asyncio.gather(*tasks, return_exceptions=True)
+    md_ist_str: list[str] = []
+    for ml in md_list:
+        if isinstance(ml, Exception):
+            logger.error(f"Failed to convert {mcpresult_pdf.model_dump()} with error {ml}")
+        else:
+            md_ist_str.append(ml)
     concatenated_md = constants.PAGE_BREAK.join(md_list)
-    stem_name = pathlib.Path(mcpresult_pdf.pdf_path).stem
-    with open(f"{stem_name}.md", "w", encoding="utf-8") as f:
+    pdf_path = pathlib.Path(mcpresult_pdf.pdf_path)
+    md_path = pdf_path.with_suffix(".md")
+    with open(md_path, "w", encoding="utf-8") as f:
         f.write(concatenated_md)
         logger.info(f"Saved markdown for {mcpresult_pdf.model_dump()} at {f.name}")
 
-def convert_pdf_list_to_markdown(semaphore: asyncio.Semaphore, mcpresult_pdf_list: list[datamodels.MCPResultsPDF], llm: LLM, sampling_params: SamplingParams):
+async def convert_pdf_list_to_markdown(semaphore: asyncio.Semaphore, mcpresult_pdf_list: list[datamodels.MCPResultsPDF], llm: AsyncLLMEngine, sampling_params: SamplingParams):
     tasks = [
         convert_single_pdf_to_markdown(semaphore, mcpresult_pdf, llm, sampling_params)
         for mcpresult_pdf in mcpresult_pdf_list
     ]
-    return asyncio.gather(*tasks)
+    return asyncio.gather(*tasks, return_exceptions=True)
 
-def pdf2md_main(mcpresult_pdf_list: list[datamodels.MCPResultsPDF]):
-    max_concurrency = os.getenv("MAX_CONCURRENCY", 2)
+async def pdf2md_main(mcpresult_pdf_list: list[datamodels.MCPResultsPDF]):
+    max_concurrency = int(os.getenv("MAX_CONCURRENCY", "2"))
     semaphore = asyncio.Semaphore(max_concurrency)
     llm, sampling_params = docling_vllm.load_vllm_model()
-    tasks = convert_pdf_list_to_markdown(semaphore, mcpresult_pdf_list, llm, sampling_params)
-    return asyncio.run(tasks)
+    return await convert_pdf_list_to_markdown(semaphore, mcpresult_pdf_list, llm, sampling_params)
 
